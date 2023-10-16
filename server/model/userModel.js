84e2ca144ad22ee08356fecd8a73207064f22d7d
Thafsi PV <pvthafsi@gmail.com>
2023-10-16 13:44:28 +0530

    userModal

---
+++ server/model/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    emailPhone: { type: String, required: true, trim: true },
+    fullName: { type: String, required: true, trim: true },
+    userName: { type: String, required: true, trim: true },
+    password: { type: String },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
