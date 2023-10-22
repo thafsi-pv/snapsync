0d28e5804311924c077dd6a3ba941544441ce078
Thafsi PV <pvthafsi@gmail.com>
2023-10-22 09:31:01 +0530

    get post api

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -3,9 +3,15 @@ const postModal = require("../model/postModel");
 const createPost = async (req, res) => {
   try {
     const userId = req.userId;
-    console.log("🚀 ~ file: postController.js:6 ~ createPost ~ userId:", userId)
+    console.log(
+      "🚀 ~ file: postController.js:6 ~ createPost ~ userId:",
+      userId
+    );
     const { media_url, caption, location } = req.body;
-    console.log("🚀 ~ file: postController.js:7 ~ createPost ~ req.body:", req.body)
+    console.log(
+      "🚀 ~ file: postController.js:7 ~ createPost ~ req.body:",
+      req.body
+    );
     if (!userId) return res.status(404).json({ message: "User not found!" });
 
     const post = { user_id: userId, media_url, caption, location };
@@ -18,4 +24,16 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const getAllPosts = async (req, res) => {
+  try {
+    const postDetails = await postModal.find();
+    if (!postDetails)
+      return res.status(404).json({ message: "No post found!" });
+    return res.status(200).json(postDetails);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+};
+
+module.exports = { createPost, getAllPosts };
--- server/router/postRouter.js
+++ server/router/postRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { checkAuth } = require("../middleware/checkAuth");
-const { createPost } = require("../controller/postController");
+const { createPost, getAllPosts } = require("../controller/postController");
 
 const postRouter = express.Router();
 
 postRouter.post("/", checkAuth, createPost);
+postRouter.get("/", getAllPosts);
 
 module.exports = postRouter;
