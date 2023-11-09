5a0adc00941bcd53f6d58cbad6691678dca4661d
Thafsi PV <pvthafsi@gmail.com>
2023-11-09 10:43:41 +0530

    story

--- client/src/layout/StoryLayout.jsx
+++ client/src/layout/StoryLayout.jsx
@@ -3,6 +3,7 @@ import logo6 from "../assets/img/snapsync_logo5.png";
 import { Link } from "react-router-dom";
 
 function StoryLayout({ children }) {
+  
   return (
     <div className="relative w-screen h-screen bg-black ">
       <Link to='/'>
