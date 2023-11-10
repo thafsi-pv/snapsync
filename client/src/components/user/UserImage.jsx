e2525a25e5f0e4625d2903a032d9756482077c0d
Thafsi PV <pvthafsi@gmail.com>
2023-11-10 23:41:18 +0530

    story

--- client/src/components/user/UserImage.jsx
+++ client/src/components/user/UserImage.jsx
@@ -3,8 +3,10 @@ import { UserActionContext } from "../../context/UserActionContext";
 import { HAVING_STORY_API } from "../../axios/const";
 import { axiosInstance } from "../../axios/axiosInterceptor";
 import StoryLoader from "../../assets/svg/StoryLoader";
+import { useNavigate } from "react-router-dom";
 
-function UserImage({ id, imgUrl, extra }) {
+function UserImage({ id, imgUrl, extra,username }) {
+  const navigate = useNavigate();
   const [haveStory, setHaveStory] = useState();
   const { loadStory } = useContext(UserActionContext);
 
@@ -19,8 +21,17 @@ function UserImage({ id, imgUrl, extra }) {
     setHaveStory(response.data.length != 0 ? true : false);
   };
 
+  const handleStoryClick = () => {
+    if (haveStory) {
+      const timeOut = setTimeout(() => {
+        navigate("/story");
+      }, 3000);
+      return () => clearTimeout(timeOut);
+    } else navigate(`/${username}`);
+  };
+
   return (
-    <div className={`circle ${extra}`}>
+    <div className={`circle cursor-pointer ${extra}`} onClick={handleStoryClick}>
       <img src={imgUrl} alt="" className={` ${extra}  `} />
       {loadStory.loading != null && haveStory && (
         <StoryLoader loadStory={loadStory} id={id} />
--- client/src/pages/homepage/components/Story.jsx
+++ client/src/pages/homepage/components/Story.jsx
@@ -7,6 +7,7 @@ import UserStory from "../../../components/user/UserStory";
 function Story() {
   const [storyList, setStoryList] = useState();
   const containerRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   const handleScroll = (scrollOffset) => {
     const container = containerRef.current;
@@ -26,34 +27,57 @@ function Story() {
     setStoryList(storyList.data);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        setScrollPosition(containerRef.current.scrollLeft);
+      }
+    };
+
+    if (containerRef.current) {
+      containerRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
   return (
     <div
       className="relative flex flex-col gap-1 max-w-full  overflow-x-auto mt-10 scrollbar-hide "
       ref={containerRef}>
       <div className="flex flex-row gap-1 items-start ml-1 mr-5 my-1 h-24">
-        <div
-          className="sticky top-9 left-5 bg-white p-1 rounded-full shadow-xl cursor-pointer hover:bg-gray-200"
-          onClick={() => handleScroll(-400)}>
-          <BiChevronLeft />
-        </div>
-        <div className="text-sm bg-red-500 rounded-full bg-gradient-to-r from-yellow-400 to-red-400 w-[70px] min-w-[70px]  h-[70px] m-2 p-[2px] ">
-          <img
+        {scrollPosition > 0 && (
+          <div
+            className="sticky top-9 left-5 bg-white p-1 rounded-full shadow-xl cursor-pointer hover:bg-gray-200 z-10"
+            onClick={() => handleScroll(-400)}>
+            <BiChevronLeft />
+          </div>
+        )}
+        <div className="text-sm w-[70px] min-w-[70px]  h-[70px] m p-[2px] ">
+          {/* <img
             className="rounded-full object-cover w-full h-full p-[2px] bg-white"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7xoV9KUFt2JC8fWh_LSYV75lFJHOVqNk-ZohGF5yoYQ&s"
             alt=""
           />
-          <p className="text-xs"> Zia_queen</p>
+          <p className="text-xs"> Zia_queen</p> */}
+          {storyList?.map((story) => (
+            <UserStory
+              id={story._id}
+              userName={story.userName}
+              imgUrl={story.imageUrl}
+            />
+          ))}
         </div>
 
-        {storyList?.map((story) => (
-          <UserStory id={story._id} userName={story.userName} imgUrl={story.imageUrl} />
-        ))}
-
-        <div
+      { scrollPosition < containerRef.current?.scrollWidth - containerRef.current?.clientWidth &&  <div
           className="sticky top-9 right-5 bg-white p-1 rounded-full shadow-xl cursor-pointer hover:bg-gray-200"
           onClick={() => handleScroll(400)}>
           <BiChevronRight />
-        </div>
+        </div>}
       </div>
     </div>
   );
@@ -61,24 +85,3 @@ function Story() {
 
 export default Story;
 
-// import React, { useRef } from 'react';
-
-// function HorizontalScrollContainer() {
-//   const containerRef = useRef(null);
-
-//   const handleScroll = (scrollOffset) => {
-//     containerRef.current.scrollLeft += scrollOffset;
-//   };
-
-//   return (
-//     <div className="flex items-center">
-//       <div ref={containerRef} className="flex overflow-x-auto space-x-4 p-4">
-//         {/* Your content goes here */}
-//       </div>
-//       <button onClick={() => handleScroll(-100)} className="px-4 py-2 bg-blue-500 text-white">Scroll Left</button>
-//       <button onClick={() => handleScroll(100)} className="px-4 py-2 bg-blue-500 text-white">Scroll Right</button>
-//     </div>
-//   );
-// }
-
-// export default HorizontalScrollContainer;
