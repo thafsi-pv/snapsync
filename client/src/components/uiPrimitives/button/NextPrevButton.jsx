fcd80b13686b0ab00e06c7dafdf85e158870a5a5
Thafsi PV <pvthafsi@gmail.com>
2023-11-29 16:10:26 +0530

    story

--- client/src/components/uiPrimitives/button/NextPrevButton.jsx
+++ client/src/components/uiPrimitives/button/NextPrevButton.jsx
@@ -4,7 +4,7 @@ import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 function NextPrevButton({ onClick, side }) {
   return (
     <div
-      className="sticky top-9 left-5 bg-white p-1 rounded-full shadow-xl cursor-pointer hover:bg-gray-200 z-10"
+      className="hidden md:sticky lg:sticky top-9 left-5 bg-white p-1 rounded-full shadow-xl cursor-pointer hover:bg-gray-200 z-10"
       onClick={() => onClick}>
       {side == "left" ? <BiChevronLeft /> : <BiChevronRight />}
     </div>
