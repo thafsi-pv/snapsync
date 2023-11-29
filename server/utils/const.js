735c304e83c0d4bfc6f20bf2f7dbb961c7cfe3b3
Thafsi PV <pvthafsi@gmail.com>
2023-11-29 14:35:04 +0530

    url

--- server/utils/const.js
+++ server/utils/const.js
@@ -1,8 +1,8 @@
-const baseUrl = "http://localhost:8090/api";
-const frondEndBaseUrl = "http://localhost:5173";
+// const baseUrl = "http://localhost:8090/api";
+// const frondEndBaseUrl = "http://localhost:5173";
 
 
-// const baseUrl = "https://snapsync-api.onrender.com/api";
-// const frondEndBaseUrl = "https://snapsync.onrender.com/";
+const baseUrl = "https://snapsync-api.onrender.com/api";
+const frondEndBaseUrl = "https://snapsync.onrender.com/";
 
 module.exports = { baseUrl, frondEndBaseUrl };
