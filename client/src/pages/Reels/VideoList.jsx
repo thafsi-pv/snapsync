f23d5dcb677cb288d627847a4d5d24d6642a4479
Thafsi PV <pvthafsi@gmail.com>
2023-12-01 12:31:07 +0530

    play

--- client/src/pages/Reels/VideoList.jsx
+++ client/src/pages/Reels/VideoList.jsx
@@ -42,6 +42,16 @@ const VideoPlayer = React.forwardRef(
       onIntersection(inView);
     }, [inView, onIntersection]);
 
+    const handleVideoClick = () => {
+      const video = videoRef.current;
+      console.log("🚀 ~ file: VideoList.jsx:47 ~ handleVideoClick ~ video:", video)
+
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    };
     return (
       <div className="relative overflow-hidden w-full  bg-black">
         <div className="w-full flex justify-center my-7">
@@ -51,14 +61,15 @@ const VideoPlayer = React.forwardRef(
                 videoRef(node);
                 ref.current = node;
               }}
-              controls={true}
+              controls
+              //onClick={handleVideoClick}
               loop={true}
               className="object-cover h-full w-fit">
               <source src={src} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-            <div className="absolute inset-0 right-0 w-full flex items-center justify-center ml-0 md:ml-14 lg:ml-14">
-              <div className="absolute  right-0 flex flex-col  h-full top-1/2 gap-6 w-16 items-end mr-3">
+            <div className=" inset-0 right-0 w-full flex items-center justify-center ml-0 md:ml-14 lg:ml-14">
+              <div className="absolute  right-0 flex flex-col  h-full top-1/2 gap-6 w-16 items-end mr-3 bg-green-500">
                 <div className="flex flex-col items-center font-semibold text-white">
                   <HeartIcon className="w-7 h-7 text-white" />
                   <p>{alldata.likeCount}</p>
