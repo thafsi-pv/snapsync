43af7bade2ad3a3a3eabddc4c8833d7018e54674
Thafsi PV <pvthafsi@gmail.com>
2023-12-07 12:20:41 +0530

    loader

--- client/src/assets/svg/Loading.jsx
+++ client/src/assets/svg/Loading.jsx
@@ -1,4 +1,8 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 export const Loading = () => {
-  return <AiOutlineLoading3Quarters class="animate-spin h-5 w-5 mr-3" />;
+  return (
+    <div className="fixed left-0 right-0 bottom-0 flex justify-center items-center h-screen w-screen bg-black bg-opacity-90 z-50">
+      <AiOutlineLoading3Quarters class="animate-spin h-5 w-5 mr-3 text-white" />
+    </div>
+  );
 };
